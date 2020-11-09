c8364b520ee6cce88d60528f0ec8f2c69d8f664a
teppi0512 <tetuhama0512@gmail.com>
2020-11-09 17:12:04 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthdate, presence: true
-
+ 
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format:{ with:/\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format:{ with:/\A[ぁ-んァ-ン一-龥]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, format:{ with:/\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name_kana, format:{ with:/\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
+    validates :birthdate
+    validates :password, format:{ with:/\A[a-zA-Z0-9]+\z/, message: "is invalid. Input full-width characters."}
+  end
 end
