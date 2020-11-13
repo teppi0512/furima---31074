c48befb924c32bf4cb9ce66dfd1c3e32360de811
teppi0512 <tetuhama0512@gmail.com>
2020-11-13 15:18:11 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @items = Item.all
-    @items = @items.includes(:user). order("created_at DESC")
+    @items = Item.all. includes(:user). order("created_at DESC")
   end
 
   def new
