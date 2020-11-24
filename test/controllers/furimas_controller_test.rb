fe18e595eb9d150152adfba50099dbd6505c0b57
teppi0512 <tetuhama0512@gmail.com>
2020-11-24 15:30:19 +0900

    11/24 15:30 修正

--- test/controllers/furimas_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FurimasControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get furimas_index_url
-    assert_response :success
-  end
-
-end
